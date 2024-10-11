c84f18cb43cc3facfc153381cd2c915ecf046690
Tyuiu.ChigurovMV@outlook.com <maxchigur@mail.ru>
2024-10-11 20:57:04 +0500

    Task7

---
+++ Tyuiu.ChigurovMV.Sprint3.Task7.V3/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.ChigurovMV.Sprint3.Task7.V3.Lib;
+
+namespace Tyuiu.ChigurovMV.Sprint3.Task7.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Чигуров М. В. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Чигуров Максим Владиславович | ПКТб-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции.           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("* Начало отрезка = " + startValue);
+            Console.WriteLine("* Конец отрезка = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |   {1, 5:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+        }
+    }
+}
